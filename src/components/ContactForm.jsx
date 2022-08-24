6403aed5eceb51740cf25fbc663a0e35bbcc078e
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-08-24 17:11:12 +0200

    the contact form is now responsive. The input fields are required, the textarea is optional. All fields are in focus when clicked. Validation is used for the email field.

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "../CSS/ContactForm.css";
 
 function ContactForm() {
+  const [developer, setDeveloper] = useState([]);
   return (
     <div className="contact-form">
       <h2>CONTACT US</h2>
       <form action="submit">
-        <select value={""}>
+        <select
+          value={developer}
+          onChange={(e) => setDeveloper(e.target.value)}
+          required
+        >
           <option value="Choose a developer">Choose a developer</option>
           <option value="adam">Adam</option>
           <option value="william">William</option>
@@ -14,9 +19,9 @@ function ContactForm() {
           <option value="miranda">Miranda</option>
           <option value="love">Love</option>
         </select>
-        <input type="text" placeholder="Name" />
-        <input type="email" name="" id="" placeholder="Email" />
-        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <input type="text" placeholder="Name" required />
+        <input type="email" placeholder="Email" required />
+        <textarea cols="30" rows="10"></textarea>
         <button type="submit">SEND</button>
       </form>
     </div>
