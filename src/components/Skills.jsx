7de24e2a53113b06f3f6e79984b7e165ab907749
William Saar <williamsaar@outlook.com>
2022-08-25 16:29:42 +0200

    made skills page responsive

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,170 @@
-import React from "react";
+import Fast from "../assets/Skills 3 icons/Blazing-fast.svg";
+import MobileFirst from "../assets/Skills 3 icons/Mobile-first.svg";
+import Learning from "../assets/Skills 3 icons/Learning.svg";
+
+import React from "../assets/Skills-icons/React.svg";
+import MongoDB from "../assets/Skills-icons/MongoDB.svg";
+import TS from "../assets/Skills-icons/TS.svg";
+import Html5 from "../assets/Skills-icons/Html5.svg";
+import Adobe from "../assets/Skills-icons/Adobe.svg";
+import Firebase from "../assets/Skills-icons/Firebase.svg";
+import NodeJS from "../assets/Skills-icons/NodeJS.svg";
+import Bootstrap from "../assets/Skills-icons/Bootstrap.svg";
+import MUI from "../assets/Skills-icons/MUI.svg";
+import Figma from "../assets/Skills-icons/Figma.svg";
+
+import { useState, useEffect } from "react";
 
 function Skills() {
-  return <div>Skills</div>;
+  const [isMobile, setIsMobile] = useState(
+    window.matchMedia("(max-width: 790px)").matches
+  );
+
+  const [isMedium, setIsMedium] = useState(
+    window.matchMedia("(max-width: 1200px)").matches
+  );
+
+  useEffect(() => {
+    window
+      .matchMedia("(max-width: 790px)")
+      .addEventListener("change", (e) => setIsMobile(e.matches));
+
+    window
+      .matchMedia("(max-width: 1200px)")
+      .addEventListener("change", (e) => setIsMedium(e.matches));
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+        height: isMedium
+          ? isMobile
+            ? "180vh"
+            : "150vh"
+          : "calc(100vh - 60px)",
+        marginBottom: "10vw",
+      }}
+    >
+      <p style={{ fontFamily: "Fredoka", fontSize: "3rem" }}>SKILLS</p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: isMobile ? "column" : "initial",
+          alignItems: isMobile ? "center" : "initial",
+          justifyContent: "center",
+        }}
+      >
+        <div style={{ textAlign: "center", width: !isMobile ? "25%" : "90%" }}>
+          <img src={Fast} alt="Blazing fast" />
+          <p
+            style={{
+              fontFamily: "Fredoka",
+              fontSize: "1.2rem",
+            }}
+          >
+            Developing blazing fast web applications for seamless user
+            interaction
+          </p>
+        </div>
+        <div style={{ textAlign: "center", width: !isMobile ? "25%" : "90%" }}>
+          <img src={MobileFirst} alt="Mobile first" />
+          <p
+            style={{
+              fontFamily: "Fredoka",
+              fontSize: "1.2rem",
+            }}
+          >
+            Mobile-first responsive design layout for all devices is a top
+            priority
+          </p>
+        </div>
+        <div style={{ textAlign: "center", width: !isMobile ? "25%" : "90%" }}>
+          <img src={Learning} alt="Always learning" />
+          <p
+            style={{
+              fontFamily: "Fredoka",
+              fontSize: "1.2rem",
+            }}
+          >
+            Constantly continues to learn new technologies and keeping up to
+            date with the freshest trends
+          </p>
+        </div>
+      </div>
+      <p style={{ fontFamily: "Fredoka", fontSize: "1.8rem" }}>
+        Click a skill to learn more
+      </p>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: !isMedium ? "1fr 1fr 1fr 1fr 1fr" : "1fr 1fr",
+
+          gap: "2rem 4rem",
+
+          justifyItems: "center",
+        }}
+      >
+        <img
+          src={React}
+          alt="React"
+          style={{ height: isMedium ? (isMobile ? "20vw" : "12vw") : "8vw" }}
+        />
+        <img
+          src={MongoDB}
+          alt="MongoDB"
+          style={{ height: isMedium ? (isMobile ? "20vw" : "12vw") : "8vw" }}
+        />
+
+        <img
+          src={TS}
+          alt="Typescript"
+          style={{ height: isMedium ? (isMobile ? "20vw" : "12vw") : "8vw" }}
+        />
+        <img
+          src={Html5}
+          alt="Html5"
+          style={{ height: isMedium ? (isMobile ? "20vw" : "12vw") : "8vw" }}
+        />
+
+        <img
+          src={Adobe}
+          alt="Adobe"
+          style={{ height: isMedium ? (isMobile ? "20vw" : "12vw") : "8vw" }}
+        />
+        <img
+          src={Firebase}
+          alt="Firebase"
+          style={{ height: isMedium ? (isMobile ? "20vw" : "12vw") : "8vw" }}
+        />
+
+        <img
+          src={NodeJS}
+          alt="NodeJS"
+          style={{ height: isMedium ? (isMobile ? "20vw" : "12vw") : "8vw" }}
+        />
+        <img
+          src={Bootstrap}
+          alt="Bootstrap"
+          style={{ height: isMedium ? (isMobile ? "20vw" : "12vw") : "8vw" }}
+        />
+
+        <img
+          src={MUI}
+          alt="Material UI"
+          style={{ height: isMedium ? (isMobile ? "20vw" : "12vw") : "8vw" }}
+        />
+        <img
+          src={Figma}
+          alt="Figma"
+          style={{ height: isMedium ? (isMobile ? "20vw" : "12vw") : "8vw" }}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
