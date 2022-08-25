0ea03a08960dd3757da79d56e3dde2fd73ce4523
Jannie87 <jannie.backman@gmail.com>
2022-08-25 13:40:28 +0200

    added stack to personas

--- src/components/Personas.jsx
+++ src/components/Personas.jsx
@@ -4,6 +4,11 @@ import William from "../assets/Pics/William.svg";
 import Jannie from "../assets/Pics/Jannie.svg";
 import Miranda from "../assets/Pics/Miranda.svg";
 import Love from "../assets/Pics/Love.svg";
+import Astack from "../assets/Stack/Astack.png";
+import Wstack from "../assets/Stack/Wstack.png";
+import Jstack from "../assets/Stack/Jstack.png";
+import Mstack from "../assets/Stack/Mstack.png";
+import Lstack from "../assets/Stack/Lstack.png";
 
 function Personas() {
   return (
@@ -11,62 +16,97 @@ function Personas() {
       <div
         style={{
           display: "flex",
-          flexDirection: "column",
           alignItems: "center",
         }}
       >
         <img alt="profile" src={Adam}></img>
-        <h3>Adam (Stefan Hawkins) Hepsever</h3>
-        <span> Frontend Developer</span>
-        <span> Stack</span>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <h3>Adam (Stefan Hawkins) Hepsever</h3>
+          <span> Frontend Developer</span>
+          <img alt="stack" src={Astack}></img>
+        </div>
       </div>
       <div
         style={{
           display: "flex",
-          flexDirection: "column",
           alignItems: "center",
         }}
       >
         <img alt="profile" src={William}></img>
-        <h3>William Arthur Saar</h3>
-        <span> Frontend Developer</span>
-        <span> Stack</span>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <h3>William Arthur Saar</h3>
+          <span> Frontend Developer</span>
+          <img alt="stack" src={Wstack}></img>
+        </div>
       </div>
       <div
         style={{
           display: "flex",
-          flexDirection: "column",
           alignItems: "center",
         }}
       >
         <img alt="profile" src={Jannie}></img>
-        <h3>Jannie Bäckman Kuurne</h3>
-        <span> Frontend Developer</span>
-        <span> Stack</span>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <h3>Jannie Bäckman Kuurne</h3>
+          <span> Frontend Developer</span>
+          <img alt="stack" src={Jstack}></img>
+        </div>
       </div>
       <div
         style={{
           display: "flex",
-          flexDirection: "column",
           alignItems: "center",
         }}
       >
         <img alt="profile" src={Miranda}></img>
-        <h3>Miranda Nilhag</h3>
-        <span> Frontend Developer</span>
-        <span> Stack</span>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <h3>Miranda Nilhag</h3>
+          <span> Frontend Developer</span>
+          <img alt="stack" src={Mstack}></img>
+        </div>
       </div>
       <div
         style={{
           display: "flex",
-          flexDirection: "column",
           alignItems: "center",
         }}
       >
         <img alt="profile" src={Love}></img>
-        <h3>Love Lanai</h3>
-        <span> Super Senior Frontend Developer</span>
-        <span> Stack</span>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <h3>Love Lanai</h3>
+          <span> Super Senior Frontend Developer</span>
+          <img alt="stack" src={Lstack}></img>
+        </div>
       </div>
     </div>
   );
