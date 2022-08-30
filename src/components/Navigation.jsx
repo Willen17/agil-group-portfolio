a43768085ba5a9d573ff7094bb507453154e1c4f
Love Lanai <love.lanai@medieinstitutet.se>
2022-08-30 21:54:01 +0200

    fixed all bugs in console to prepare for netlify launch

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -34,22 +34,22 @@ export default function Navigation() {
           <ThemeProvider theme={theme}>
             <ul className="dotnav dotnav-vertical dotnav-right nav">
               <Tooltip placement="right-end" title="Top">
-                <li className="tooltiptext" title="Top">
+                <li className="tooltiptext">
                   <a href="#start"></a>
                 </li>
               </Tooltip>
               <Tooltip placement="right-end" title="About">
-                <li className="nav-tooltip" title="About">
+                <li className="nav-tooltip">
                   <a href="#about"></a>
                 </li>
               </Tooltip>
               <Tooltip placement="right-end" title="Competences">
-                <li className="nav-tooltip" title="Competences">
+                <li className="nav-tooltip">
                   <a href="#competences"></a>
                 </li>
               </Tooltip>
               <Tooltip placement="right-end" title="Contact">
-                <li className="nav-tooltip" title="Contact">
+                <li className="nav-tooltip">
                   <a href="#contact"></a>
                 </li>
               </Tooltip>
@@ -57,32 +57,7 @@ export default function Navigation() {
           </ThemeProvider>
         </div>
       ) : (
-        <div>
-          {/* <ThemeProvider theme={theme}>
-          <ul className="dotnav-mobile dotnav-vertical-mobile dotnav-mobile nav">
-            <Tooltip placement="bottom" title="Top">
-              <li className="tooltiptext" title="Top">
-                <a href="#start"></a>
-              </li>
-            </Tooltip>
-            <Tooltip placement="bottom" title="About">
-              <li className="nav-tooltip" title="About">
-                <a href="#about"></a>
-              </li>
-            </Tooltip>
-            <Tooltip placement="bottom" title="Competences">
-              <li className="nav-tooltip" title="Competences">
-                <a href="#competences"></a>
-              </li>
-            </Tooltip>
-            <Tooltip placement="bottom" title="Contact">
-              <li className="nav-tooltip" title="Contact">
-                <a href="#contact"></a>
-              </li>
-            </Tooltip>
-          </ul>
-        </ThemeProvider> */}
-        </div>
+        <div></div>
       )}
     </div>
   );
