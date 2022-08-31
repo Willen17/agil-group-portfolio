4f9b434c3427721e4b94b3dd61e4200ccb5becdb
Love Lanai <love.lanai@medieinstitutet.se>
2022-08-31 13:01:40 +0200

    fixed login functions and loginmodal/usermodal

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,21 @@ import Header from "./components/Header.jsx";
 import StartPage from "./components/StartPage.jsx";
 import About from "./components/About.jsx";
 import Personas from "./components/Personas";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Navigation from "components/Navigation.jsx";
 import Skills from "./components/Skills.jsx";
 import Map from "./components/Map.jsx";
 import Footer from "./components/Footer.jsx";
 import ContactForm from "./components/ContactForm.jsx";
-import Login from "./components/Login.jsx";
-
 import "./CSS/App.css";
-import "./CSS/Login.css";
 
 function App() {
-  const [isShowLogin, setIsShowLogin] = useState(false);
-
-  const handleLoginClick = () => {
-    setIsShowLogin((isShowLogin) => !isShowLogin);
-  };
-
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }, [isShowLogin]);
-
   return (
     <div className="App">
       <Navigation />
       <section id="start">
-        <Header handleLoginClick={handleLoginClick}></Header>
-        <Login isShowLogin={isShowLogin} />
+        <Header />
+
         <StartPage />
       </section>
 
